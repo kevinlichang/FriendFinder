133842053e4e4eebe3e39e3a91b1efac336a5677
Kchar <38297595+kevinlichang@users.noreplay.github.com>
2018-10-16 22:06:46 -0400

    added matching functionality to post method

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,6 +3,16 @@ const friendsArr = [
     name: "Harry",
     photo: "http://www.placecage.com/200/300",
     scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+  },
+  {
+    name: "Ron",
+    photo: "http://www.placecage.com/300/300",
+    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+  },
+  {
+    name: "Hermione",
+    photo: "http://www.placecage.com/300/300",
+    scores: [5, 5, 2, 2, 2, 5, 5, 5, 5, 5]
   }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,9 +9,37 @@ module.exports = function(app) {
   app.post("/api/friends", function(req, res) {
     const newFriend = req.body;
 
+    const userScore = newFriend.scores;
+
+    let match = {};
+    let totalDiffNum = 50;
+
+    
+
+
+    for (let i = 0; i < friendsData.length; i++) {
+      const potentialFriendScore = friendsData[i].scores;
+      const scoreDiffArr = [];
+      
+      for (let x = 0; x < userScore.length; x++) {
+        let scoreDiff = Math.abs(userScore[x] - potentialFriendScore[x]);
+        scoreDiffArr.push(scoreDiff);
+      }; 
+
+
+      // add all the scoreDiffArr elements together for total Difference
+      const sum = scoreDiffArr.reduce((a, b) => a + b, 0);
+
+      if (sum < totalDiffNum) {
+        totalDiffNum = sum;
+        match = friendsData[i];
+      }
+
+    }
+
     friendsData.push(newFriend);
 
-    res.json(newFriend);
+    res.json(match);
   });
 
 
