1bfac286ec82e283faa193e23ee15aa228e89ca5
Kchar <38297595+kevinlichang@users.noreplay.github.com>
2018-10-17 16:46:53 -0400

    updated survey.html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,23 @@
 const friendsArr = [
   {
     name: "Harry",
-    photo: "http://www.placecage.com/200/300",
+    photo: "https://vignette.wikia.nocookie.net/harrypotter/images/b/ba/Harry-Potter-1-.jpg/revision/latest?cb=20090503181116&path-prefix=fi",
     scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
   },
   {
     name: "Ron",
-    photo: "http://www.placecage.com/300/300",
+    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/5/5e/Ron_Weasley_poster.jpg/220px-Ron_Weasley_poster.jpg",
     scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
   },
   {
     name: "Hermione",
-    photo: "http://www.placecage.com/300/300",
+    photo: "https://i.pinimg.com/236x/21/7d/01/217d015bb0073b011285b86d4ae1ae38--harry-potter-hermione-harry-potter-film.jpg",
     scores: [5, 5, 2, 2, 2, 5, 5, 5, 5, 5]
+  },
+  {
+    name: "Neville",
+    photo: "http://i.cdn.turner.com/cnn/2011/SHOWBIZ/07/15/matthew.lewis.neville/t1larg.matt.jpg",
+    scores: [4, 4, 3, 3, 2, 1, 1, 2, 5, 3]
   }
 ];
 
